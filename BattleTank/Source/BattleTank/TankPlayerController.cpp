7ebdd7c669758f2188598c54009f4b36f3036ffe
Taidio <przemex14@gmail.com>
2017-10-10 23:27:47 +0200

    Overriding BeginPlay method from Aactor

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -5,7 +5,25 @@
 
 
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController begin play"))
+	if (GetControlledTank())
+	{
+		auto PossessedTank = GetControlledTank();
+		UE_LOG(LogTemp, Warning, TEXT("You are possessing %s"),*PossessedTank->GetName())
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("You are possessing nothing"))
+	}
+
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
+
+
