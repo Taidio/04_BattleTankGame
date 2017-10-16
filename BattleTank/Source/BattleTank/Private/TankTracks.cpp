d8d436da35e7cc92fdbb8e669a454784545fa2db
Taidio <przemex14@gmail.com>
2017-10-17 00:09:47 +0200

    ApplyForceAtLocation with some bugs(yet)

---
+++ BattleTank/Source/BattleTank/Private/TankTracks.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTracks.h"
+
+
+
+
+void UTankTracks::SetThrottle(float Throttle)
+{
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	//aplying force at TankRoot primitive component
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+
+}
---
+++ BattleTank/Source/BattleTank/Public/TankTracks.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTracks.generated.h"
+
+/**
+ * 
+ */
+UCLASS(meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UTankTracks : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+	
+	//Max Force on Tracks in Newtons
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000; // assuming 40t Tank and 1g acceleration
+
+};
